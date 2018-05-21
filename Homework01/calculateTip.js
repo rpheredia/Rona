90aaee54995c697522d21c328f18e5acb3534e40
Rona User <mafredi@RONA-LENOVO.bblbze.com>
2018-05-21 14:48:56 -0600

    re-format file structure

--- Homework01/calculateTip.js
+++
@@ -1,9 +0,0 @@
-function calculateTip(total) {
-    var tipPercent = 0.15;
-    return (total * tipPercent);
-}
-
-var billTotal = 10.00;
-var billTip =calculateTip(billTotal);
-var receipt = 'Total: ' + billTotal + ' Tip: ' + billTip;
-console.log(receipt);
--- Homework01/fortune.js
+++
@@ -1,5 +0,0 @@
-var children = 4;
-var pname = "Joe Black";
-var glocation = "Belize";
-var title = "Doctor";
-console.log("You will be a " + title + " in " + glocation + ", and married to " + pname + " with " + children + " kids");
